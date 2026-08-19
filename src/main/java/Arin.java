0f093e4753f1d22470bb943db53470ae3a57e8c0
Wei Chong <101.21.gohweichong@gmail.com>
2026-08-19 18:31:31 +0800

    Add Greeting

--- src/main/java/Arin.java
+++ src/main/java/Arin.java
@@ -1,10 +1,16 @@
 public class Arin {
     public static void main(String[] args) {
-        String banner = "    _         _       \n" +
+        String banner = "________________________________\n" +
+                "    _         _       \n" +
                 "   / \\   _ __(_)_ __  \n" +
                 "  / _ \\ | '__| | '_ \\ \n" +
                 " / ___ \\| |  | | | | |\n" +
-                "/_/   \\_\\_|  |_|_| |_|";
+                "/_/   \\_\\_|  |_|_| |_|\n" +
+                "Hello! I'm Arin.\n" +
+                "What can I do for you?\n" +
+                "________________________________\n" +
+                "Bye. Hope to see you again soon!";
+
         System.out.println(banner);
     }
 }
