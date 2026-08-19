f36e72c97b26b9ded4b1b1d7c9d6f2865d6a00e5
Wei Chong <101.21.gohweichong@gmail.com>
2026-08-19 18:52:20 +0800

    Fixed bug in echo

--- src/main/java/Arin.java
+++ src/main/java/Arin.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * A chatbot that echoes commands until the user ends the conversation.
+ */
 public class Arin {
+    /**
+     * Starts Arin and processes commands from standard input.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
         String banner = "________________________________\n" +
                 "    _         _       \n" +
@@ -6,11 +16,29 @@ public class Arin {
                 "  / _ \\ | '__| | '_ \\ \n" +
                 " / ___ \\| |  | | | | |\n" +
                 "/_/   \\_\\_|  |_|_| |_|\n" +
-                "Hi! I'm Arin.\n" +
-                "What can I do for you?\n" +
-                "________________________________\n" +
-                "Bye. Hope to see you again soon!";
+                "Good afternoon.\n"
+                + "My name is Arin.\n"
+                + "And I am a Wilderness Explorer in Tribe 54, Sweat Lodge 12.\n"
+                + "Are you in need of any assistance today, sir?\n" +
+                "________________________________";
 
         System.out.println(banner);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                break;
+            }
+
+            System.out.println("________________________________");
+            System.out.println("    " + command);
+            System.out.println("________________________________");
+        }
+
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
     }
 }
