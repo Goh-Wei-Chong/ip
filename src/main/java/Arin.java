8d1703e37c5457cc06044633440f9c556e0481f8
Wei Chong <101.21.gohweichong@gmail.com>
2026-08-19 18:15:44 +0800

    Renamed Duke.java to Arin.java and chaged the ascii to my Arin

---
+++ src/main/java/Arin.java
@@ -0,0 +1,10 @@
+public class Arin {
+    public static void main(String[] args) {
+        String banner = "    _         _       \n" +
+                "   / \\   _ __(_)_ __  \n" +
+                "  / _ \\ | '__| | '_ \\ \n" +
+                " / ___ \\| |  | | | | |\n" +
+                "/_/   \\_\\_|  |_|_| |_|";
+        System.out.println(banner);
+    }
+}
