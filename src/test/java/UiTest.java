3dc68950fa9ebe00b458cf95c8b2843843146e24
Wei Chong <101.21.gohweichong@gmail.com>
2026-08-26 16:15:39 +0800

    Extract console interaction into Ui class
    Move command input and all user-facing messages out of Arin so its remaining responsibilities can be separated into Parser and TaskList in later increments. Add a focused Ui regression test while preserving the existing command output.

---
+++ src/test/java/UiTest.java
@@ -0,0 +1,34 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Regression checks for UI messages used by Arin.
+ */
+public class UiTest {
+    /**
+     * Verifies that the UI reads commands and labels list entries correctly.
+     *
+     * @param args command-line arguments, which are not used
+     */
+    public static void main(String[] args) {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        Ui ui = new Ui(new Scanner("todo read book\n"), new PrintStream(buffer));
+
+        if (!"todo read book".equals(ui.readCommand())) {
+            throw new AssertionError("Ui did not read the entered command.");
+        }
+
+        ui.showTaskList(List.of(new Todo("read book")));
+        String expected = "________________________________" + System.lineSeparator()
+                + "Here are the tasks in your list:" + System.lineSeparator()
+                + "1. [T][ ] read book" + System.lineSeparator()
+                + "________________________________" + System.lineSeparator();
+        if (!expected.equals(buffer.toString())) {
+            throw new AssertionError("Unexpected list output: " + buffer);
+        }
+
+        System.out.println("UiTest passed");
+    }
+}
